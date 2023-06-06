0b0963e259a25df391eaa74c41b300548f023a07
Chris Ormandy <chrisormandy471@googlemail.com>
2023-06-06 22:00:59 +0100

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# CEML Design Goals
+
+1. Expression templated, GPU and CPU locatable linear algebra + Deep learning library
+2. Support for computing gradients
+3. Load a model definition at compile time, to achieve minimum memory footprint and maximum performance.
+
+# Objecives
+1. No allocation during running of network
+2. No polymorphism
+3. CUDA support
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "ceml/tensor.hpp"
+#include "ceml/operations.hpp"
+#include "include/ceml/tensor.hpp"
+
+int main(){
+    ceml::Tensor<double, 5, 1, 1> x;
+    ceml::Tensor<double, 5, 1, 1> y;
+    
+    x.allocate();
+    y.allocate();
+    for(uint i=0; i<x.size1; i++){
+        x.assign(i, 0, 0, 1);
+        y.assign(i, 0, 0, 1);
+    }
+    
+    // auto z = ceml::ewiseMult<double>(ceml::add<double>(ceml::add<double>(x,y), x), y);
+    
+    auto z =ceml::add<double>(ceml::add<double>(x,y), y);
+
+
+   std::cout<<x.size1<<std::endl;
+   std::cout<<y.size1<<std::endl;
+   std::cout<<z.size1<<std::endl;
+   
+   auto res = z.materialise();
+
+  std::cout<<z(0,0,0)<<std::endl; 
+    
+return 0;
+
+}
